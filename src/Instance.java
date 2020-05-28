4096e9cd27f18995390dbbb8c60b8156cfebb1a3
Sergio Hernandez Dominguez <sergiohd47@gmail.com>
2020-05-28 13:43:41 +0200

    Summary

---
+++ src/Instance.java
@@ -0,0 +1,218 @@
+import EstructuraDatos.Grafo;
+import EstructuraDatos.GrafoND;
+import EstructuraDatos.Triple;
+
+
+import java.io.*;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Sergio Hernandez Dominguez
+ */
+public class Instance {
+    private int numeroNodos;
+    private int nodoMaximo;
+    private List<Double> listaProbabilidades;
+
+    public int getNumeroNodos() {
+        return numeroNodos;
+    }
+
+    public int getNodoMaximo() {
+        return nodoMaximo;
+    }
+
+    public Instance(){
+        this.numeroNodos=0;
+        this.listaProbabilidades=Arrays.asList(0.1,0.01,0.001);
+
+    }
+    public String modificarFichero(String rutaFicheroOriginal){
+        String patronTXT=".*.txt.*";
+        String patronCSV=".*.csv.*";
+        Pattern patTXT=Pattern.compile(patronTXT);
+        Pattern patCSV=Pattern.compile(patronCSV);
+        Matcher matTXT=patTXT.matcher(rutaFicheroOriginal);
+        Matcher matCSV=patCSV.matcher(rutaFicheroOriginal);
+        File archivo=null;
+        FileReader fr=null;
+        BufferedReader br=null;
+        File ficheroEscribir=null;
+        FileWriter ficheroEscribirAux=null;
+        PrintWriter pw=null;
+        if(matTXT.matches()) {
+            try {
+                archivo = new File(rutaFicheroOriginal);
+                String nombreArchivoOrig=archivo.getName().split(".txt")[0];
+                String rutaArchivoOrig=archivo.getPath();
+                fr = new FileReader(archivo);
+                br = new BufferedReader(fr);
+                //EL VALOR DE 4 SE CORRESPONDE CON LA LONGITUD DE LA REGEX .txt
+                ficheroEscribir= new File((rutaArchivoOrig.substring(0,rutaArchivoOrig.length()-(nombreArchivoOrig.length()+4)))+nombreArchivoOrig+"ConPeso.txt");
+                ficheroEscribirAux=new FileWriter(ficheroEscribir.getPath());
+                pw=new PrintWriter(ficheroEscribir);
+                String linea;
+                Random randomPick=new Random();
+                pw.println("#FICHERO SIMILAR AL FICHERO: "+nombreArchivoOrig+".txt SOLO QUE SE LE AÑADEN PESOS A LOS ARCOS");
+                pw.println("#nodoOrigen nodoDestino pesoArco");
+                while ((linea = br.readLine()) != null) {
+                    if (linea.indexOf("#") == 0) { //Ignoran las lineas que empiezan por "#"
+                        continue;
+                    }
+                    int nodoOrigen = Integer.parseInt(linea.split("\\s+")[0]);
+                    int nodoDestino = Integer.parseInt(linea.split("\\s+")[1]);
+                    double pesoArco = this.listaProbabilidades.get(randomPick.nextInt(this.listaProbabilidades.size()));
+                    pw.println(nodoOrigen+" "+nodoDestino+" "+pesoArco);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (fr != null) {
+                        fr.close();
+                    }
+                    if (ficheroEscribir!=null){
+                        ficheroEscribirAux.close();
+                    }
+                } catch (Exception e2) {
+                    e2.printStackTrace();
+                }
+            }
+        }else if(matCSV.matches()){
+            try {
+                archivo = new File(rutaFicheroOriginal);
+                String nombreArchivoOrig=archivo.getName().split(".csv")[0];
+                String rutaArchivoOrig=archivo.getPath();
+                fr = new FileReader(archivo);
+                br = new BufferedReader(fr);
+                //EL VALOR DE 4 SE CORRESPONDE CON LA LONGITUD DE LA REGEX .txt
+                ficheroEscribir=new File((rutaArchivoOrig.substring(0,rutaArchivoOrig.length()-(nombreArchivoOrig.length()+4)))+nombreArchivoOrig+"ConPeso.csv");
+                ficheroEscribirAux=new FileWriter(ficheroEscribir.getPath());
+                pw=new PrintWriter(ficheroEscribir);
+                String linea;
+                Random randomPick=new Random();
+                pw.println("#FICHERO SIMILAR AL FICHERO: "+nombreArchivoOrig+".csv SOLO QUE SE LE AÑADEN PESOS A LOS ARCOS");
+                pw.println("#nodoOrigen,nodoDestino,pesoArco");
+                while ((linea=br.readLine()) != null) {
+                    if (linea.indexOf("#") == 0) { //Ignoran las lineas que empiezan por "#"
+                        continue;
+                    }
+                    //EN LA REGEX DEL FICHERO DE PRUEBA CREADO POR MI, ES NECESARIO PONER "\t".
+                    //EN LA REGEX DEL FICHERO DE SNAP "snap2/email-Eu-core.txt", LA REGEX ES UN ESPACIO " ".
+                    //REGEX: "\\s+" numero de espacios que sean
+                    int nodoOrigen = Integer.parseInt(linea.split(",")[0]);
+                    int nodoDestino = Integer.parseInt(linea.split(",")[1]);
+                    double pesoArco = this.listaProbabilidades.get(randomPick.nextInt(this.listaProbabilidades.size()));
+                    pw.println(nodoOrigen+","+nodoDestino+","+pesoArco);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (fr != null) {
+                        fr.close();
+                    }
+                } catch (Exception e2) {
+                    e2.printStackTrace();
+                }
+            }
+        }
+        return ficheroEscribir.getPath();
+    }
+    public ArrayList<Triple<Integer,Integer, Double>> leerFicheroYaModificado(String rutaFicheroModif){
+        //SE USA EL SISTEMA DE PATRONES Y DE REGEX PARA DISTINGUIR ENTRE DOS TIPOS DE FICHEROS, LOS .txt y LOS .csv
+        String patronTXT=".*.txt.*";
+        String patronCSV=".*.csv.*";
+        Pattern patTXT=Pattern.compile(patronTXT);
+        Pattern patCSV=Pattern.compile(patronCSV);
+        Matcher matTXT=patTXT.matcher(rutaFicheroModif);
+        Matcher matCSV=patCSV.matcher(rutaFicheroModif);
+        HashSet<Integer> nodos=new HashSet<>();
+        ArrayList<Triple<Integer,Integer,Double>> listaNodos= new ArrayList<>();
+        File archivo=null;
+        FileReader fr=null;
+        BufferedReader br=null;
+        if(matTXT.matches()) {
+            try {
+                archivo = new File(rutaFicheroModif);
+                fr = new FileReader(archivo);
+                br = new BufferedReader(fr);
+                String linea;
+                while ((linea = br.readLine()) != null) {
+                    if (linea.indexOf("#") == 0) { //Ignoran las lineas que empiezan por "#"
+                        continue;
+                    }
+                    int nodoOrigen = Integer.parseInt(linea.split("\\s+")[0]);
+                    nodos.add(nodoOrigen);
+                    int nodoDestino = Integer.parseInt(linea.split("\\s+")[1]);
+                    nodos.add(nodoDestino);
+                    double pesoArco= Double.parseDouble(linea.split("\\s+")[2]);
+                    listaNodos.add(new Triple<>(nodoOrigen, nodoDestino, pesoArco)); //lista de pares de nodoOrigen, nodoDestino y peso de cada arco.
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (fr != null) {
+                        fr.close();
+                    }
+                } catch (Exception e2) {
+                    e2.printStackTrace();
+                }
+            }
+        }else if(matCSV.matches()){
+            try {
+                archivo = new File(rutaFicheroModif);
+                fr = new FileReader(archivo);
+                br = new BufferedReader(fr);
+                String linea;
+                while ((linea = br.readLine()) != null) {
+                    if (linea.indexOf("#") == 0) { //Ignoran las lineas que empiezan por "#"
+                        continue;
+                    }
+                    //EN LA REGEX DEL FICHERO DE PRUEBA CREADO POR MI, ES NECESARIO PONER "\t".
+                    //EN LA REGEX DEL FICHERO DE SNAP "snap2/email-Eu-core.txt", LA REGEX ES UN ESPACIO " ".
+                    //REGEX: "\\s+" numero de espacios que sean
+                    System.out.println(linea);
+                    int nodoOrigen = Integer.parseInt(linea.split(",")[0]);
+                    nodos.add(nodoOrigen);
+                    int nodoDestino = Integer.parseInt(linea.split(",")[1]);
+                    nodos.add(nodoDestino);
+                    double pesoArco = Double.parseDouble(linea.split(",")[2]);
+                    listaNodos.add(new Triple<>(nodoOrigen, nodoDestino,pesoArco)); //lista de pares de nodoOrigen, nodoDestino y peso de cada arco.
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (fr != null) {
+                        fr.close();
+                    }
+                } catch (Exception e2) {
+                    e2.printStackTrace();
+                }
+            }
+        }
+        this.numeroNodos=nodos.size();
+        this.nodoMaximo= Collections.max(nodos);
+        return listaNodos;
+    }
+    public  Grafo construirGrafo(ArrayList<Triple<Integer, Integer, Double>> listaNodos) throws RuntimeException{
+        if(listaNodos.isEmpty()){
+            throw new RuntimeException("Lista nodos vacia");
+        }
+        Grafo grafoND=new GrafoND(this.numeroNodos,this.nodoMaximo);
+        for(Triple<Integer, Integer, Double> tripleValores: listaNodos){
+            if(!grafoND.nodos().contains(tripleValores.getPrimerElemento())){
+                grafoND.insertarNodo(tripleValores.getPrimerElemento());
+            }
+            if(!grafoND.nodos().contains(tripleValores.getSegundoElemento())){
+                grafoND.insertarNodo(tripleValores.getSegundoElemento());
+            }
+            grafoND.insertarArco(tripleValores.getPrimerElemento(), tripleValores.getSegundoElemento(), tripleValores.getTercerElemento());
+        }
+        return grafoND;
+    }
+}
--- src/Principal.java
+++ src/Principal.java
@@ -1,3 +1,9 @@
+import EstructuraDatos.Grafo;
+import EstructuraDatos.GrafoND;
+import EstructuraDatos.Triple;
+
+import java.util.ArrayList;
+
 /**
  * @author Sergio Hernandez Dominguez
  */
@@ -14,22 +20,28 @@ public class Principal {
 
     //              RUTAS SNAP
     //      FORMATO .TXT
-    private static final String RUTA_COLLEGEMSG_1899="/Users/sergiohernandezdominguez/Desktop/universidad/TFG/SNAP/formatoTXT/snapCollegeMsg/CollegeMsg.txt";
-    private static final String RUTA_EMAILEU_1005="/Users/sergiohernandezdominguez/Desktop/universidad/TFG/SNAP/formatoTXT/snapEmailEU/email-Eu-core.txt";
-    private static final String RUTA_FACEBOOKCOMB_4039="/Users/sergiohernandezdominguez/Desktop/universidad/TFG/SNAP/formatoTXT/snapFacebComb/facebook_combined.txt";
-    private static final String RUTA_GNUTELLA5_8846="/Users/sergiohernandezdominguez/Desktop/universidad/TFG/SNAP/formatoTXT/snapGNutella5/p2p-Gnutella05.txt";
-    private static final String RUTA_GNUTELLA6_8717="/Users/sergiohernandezdominguez/Desktop/universidad/TFG/SNAP/formatoTXT/snapGNutella6/p2p-Gnutella06.txt";
-    private static final String RUTA_GNUTELLA8_6301="/Users/sergiohernandezdominguez/Desktop/universidad/TFG/SNAP/formatoTXT/snapGNutella8/p2p-Gnutella08.txt";
-    private static final String RUTA_GNUTELLA9_8114="/Users/sergiohernandezdominguez/Desktop/universidad/TFG/SNAP/formatoTXT/snapGNutella9/p2p-Gnutella09.txt";
-    private static final String RUTA_GRQC_5242="/Users/sergiohernandezdominguez/Desktop/universidad/TFG/SNAP/formatoTXT/snapGRQC/CA-GrQc.txt";
-    private static final String RUTA_WIKIVOTE_7115="/Users/sergiohernandezdominguez/Desktop/universidad/TFG/SNAP/formatoTXT/snapWikiVote/Wiki-Vote.txt";
+    private static final String RUTA_COLLEGEMSG_1899="/Users/sergiohernandezdominguez/Desktop/universidad/TFG2/SNAP/formatoTXT/snapCollegeMsg/CollegeMsg.txt";
+    private static final String RUTA_EMAILEU_1005="/Users/sergiohernandezdominguez/Desktop/universidad/TFG2/SNAP/formatoTXT/snapEmailEU/email-Eu-core.txt";
+    private static final String RUTA_FACEBOOKCOMB_4039="/Users/sergiohernandezdominguez/Desktop/universidad/TFG2/SNAP/formatoTXT/snapFacebComb/facebook_combined.txt";
+    private static final String RUTA_GNUTELLA5_8846="/Users/sergiohernandezdominguez/Desktop/universidad/TFG2/SNAP/formatoTXT/snapGNutella5/p2p-Gnutella05.txt";
+    private static final String RUTA_GNUTELLA6_8717="/Users/sergiohernandezdominguez/Desktop/universidad/TFG2/SNAP/formatoTXT/snapGNutella6/p2p-Gnutella06.txt";
+    private static final String RUTA_GNUTELLA8_6301="/Users/sergiohernandezdominguez/Desktop/universidad/TFG2/SNAP/formatoTXT/snapGNutella8/p2p-Gnutella08.txt";
+    private static final String RUTA_GNUTELLA9_8114="/Users/sergiohernandezdominguez/Desktop/universidad/TFG2/SNAP/formatoTXT/snapGNutella9/p2p-Gnutella09.txt";
+    private static final String RUTA_GRQC_5242="/Users/sergiohernandezdominguez/Desktop/universidad/TFG2/SNAP/formatoTXT/snapGRQC/CA-GrQc.txt";
+    private static final String RUTA_WIKIVOTE_7115="/Users/sergiohernandezdominguez/Desktop/universidad/TFG2/SNAP/formatoTXT/snapWikiVote/Wiki-Vote.txt";
     //      FORMATO .CSV
-    private static final String RUTA_BITCOINOTC_5881="/Users/sergiohernandezdominguez/Desktop/universidad/TFG/SNAP/formatoCSV/snapBitcoinOTC/soc-sign-bitcoinotc.csv";
-    private static final String RUTA_BITCOINALPHA_3783="/Users/sergiohernandezdominguez/Desktop/universidad/TFG/SNAP/formatoCSV/snapBitcoinAlpha/soc-sign-bitcoinalpha.csv";
-    private static final String RUTA_GOVERMENT_7057="/Users/sergiohernandezdominguez/Desktop/universidad/TFG/SNAP/formatoCSV/snapGoverment/government_edges.csv";
-    private static final String RUTA_TVSHOW_3892="/Users/sergiohernandezdominguez/Desktop/universidad/TFG/SNAP/formatoCSV/snapTVShow/tvshow_edges.csv";
-    private static final String RUTA_POLITICIAN_5908="/Users/sergiohernandezdominguez/Desktop/universidad/TFG/SNAP/formatoCSV/snapPolitician/politician_edges.csv";
-    public static void main(String[] args) {
+    private static final String RUTA_BITCOINOTC_5881="/Users/sergiohernandezdominguez/Desktop/universidad/TFG2/SNAP/formatoCSV/snapBitcoinOTC/soc-sign-bitcoinotc.csv";
+    private static final String RUTA_BITCOINALPHA_3783="/Users/sergiohernandezdominguez/Desktop/universidad/TFG2/SNAP/formatoCSV/snapBitcoinAlpha/soc-sign-bitcoinalpha.csv";
+    private static final String RUTA_GOVERMENT_7057="/Users/sergiohernandezdominguez/Desktop/universidad/TFG2/SNAP/formatoCSV/snapGoverment/government_edges.csv";
+    private static final String RUTA_TVSHOW_3892="/Users/sergiohernandezdominguez/Desktop/universidad/TFG2/SNAP/formatoCSV/snapTVShow/tvshow_edges.csv";
+    private static final String RUTA_POLITICIAN_5908="/Users/sergiohernandezdominguez/Desktop/universidad/TFG2/SNAP/formatoCSV/snapPolitician/politician_edges.csv";
 
+    public static void main(String[] args) {
+        Instance instancia=new Instance();
+        String ruta=instancia.modificarFichero(RUTA_TVSHOW_3892);
+        ArrayList<Triple<Integer,Integer,Double>> listaNodos=instancia.leerFicheroYaModificado(ruta);
+        //Grafo grafoND=instancia.construirGrafo(listaNodos);
+        System.out.println("---------------");
+        //System.out.println(grafoND.tamañoGrafo());
     }
 }
